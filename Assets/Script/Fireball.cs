963fa702a2b6ef159c4f6919d4b14997fd811ea3
Alexander Chumovitsky <alexander.chumovitsky@identigy.com>
2020-05-10 23:10:23 +0300

    complete lession 3

---
+++ Assets/Script/Fireball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int damage = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCharacter player = other.GetComponent<PlayerCharacter>();
+        if (null != player)
+        {
+            player.hurt(1);
+        }
+        Destroy(this.gameObject);
+    }
+}
